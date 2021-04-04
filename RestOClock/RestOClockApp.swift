3831cd4c52bc9af6ca08abd3da61434ddc27188c
ae2u16 <ae2u16@soton.ac.uk>
2021-04-04 16:18:07 +0100

    Initial Commit

---
+++ RestOClock/RestOClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestOClockApp.swift
+//  RestOClock
+//
+//  Created by Arthur Edelmans on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct RestOClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
